d17a32166d2bd5451eadaf73d49427bdde11bbd7
masterUNG <phrombutr@gmail.com>
2016-05-24 12:30:34 +0700

    Create SQLite

---
+++ app/src/main/java/appewtc/masterung/bookmeetingpbru/MyOpenHelper.java
@@ -0,0 +1,37 @@
+package appewtc.masterung.bookmeetingpbru;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by masterUNG on 5/24/16 AD.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+    //Explicit
+    public static final String database_name = "Pbru.db";
+    private static final int database_version = 1;
+    private static final String create_user_table = "create table userTABLE (" +
+            "_id integer primary key," +
+            "Name text," +
+            "Surname text," +
+            "IDcard text," +
+            "Office text," +
+            "User text," +
+            "Password text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null,database_version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_user_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}   // Main Class
