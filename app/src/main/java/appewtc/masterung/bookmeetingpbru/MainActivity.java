e5cfab43b46a7b90b28afe1cf83872f77166f8ed
masterUNG <phrombutr@gmail.com>
2016-05-24 14:47:03 +0700

    Async

--- app/src/main/java/appewtc/masterung/bookmeetingpbru/MainActivity.java
+++ app/src/main/java/appewtc/masterung/bookmeetingpbru/MainActivity.java
@@ -1,14 +1,22 @@
 package appewtc.masterung.bookmeetingpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private String strURL = "http://swiftcodingthai.com/pbru/get_user.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,56 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(this);
 
+        //Test Add User
+        //myManage.addUser("name", "sur", "idcard", "Off", "user", "pass");
+
+        //Delete All SQLite
+        deleteAllSQLite();
+
+        synJSON();
+
     }   // Main Method
+
+    private void synJSON() {
+        SynUser synUser = new SynUser();
+        synUser.execute();
+    }
+
+    //Create Inner Class
+    public class SynUser extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strURL).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("pbruV1", "strJSON ==> " + s);
+
+        }   // onPost
+    }   // SynUser Class
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
 
     public void clickSingUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SingUpActivity.class));
