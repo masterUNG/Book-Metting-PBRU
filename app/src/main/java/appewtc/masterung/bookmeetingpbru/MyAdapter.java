8cabeafb905d146c2bddd3f11af881d1c0f99aff
masterUNG <phrombutr@gmail.com>
2016-05-25 14:40:54 +0700

    Read JsON

---
+++ app/src/main/java/appewtc/masterung/bookmeetingpbru/MyAdapter.java
@@ -0,0 +1,74 @@
+package appewtc.masterung.bookmeetingpbru;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by masterUNG on 5/25/16 AD.
+ */
+public class MyAdapter extends BaseAdapter {
+
+    private Context context;
+    private String[] nameRoomStrings, nameBuildStrings, sizeStrings,
+            priceDayStrings, priceHolidayStrings, iconStrings;
+
+    public MyAdapter(Context context,
+                     String[] nameRoomStrings,
+                     String[] nameBuildStrings,
+                     String[] sizeStrings,
+                     String[] priceDayStrings,
+                     String[] priceHolidayStrings,
+                     String[] iconStrings) {
+        this.context = context;
+        this.nameRoomStrings = nameRoomStrings;
+        this.nameBuildStrings = nameBuildStrings;
+        this.sizeStrings = sizeStrings;
+        this.priceDayStrings = priceDayStrings;
+        this.priceHolidayStrings = priceHolidayStrings;
+        this.iconStrings = iconStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameRoomStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        TextView nameRoomTextView = (TextView) view1.findViewById(R.id.textView8);
+        nameRoomTextView.setText(nameRoomStrings[i]);
+
+        TextView nameBuildTextView = (TextView) view1.findViewById(R.id.textView9);
+        nameBuildTextView.setText(nameBuildStrings[i]);
+
+        TextView sizeTextView = (TextView) view1.findViewById(R.id.textView10);
+        sizeTextView.setText(sizeStrings[i]);
+
+        TextView priceDayTextView = (TextView) view1.findViewById(R.id.textView11);
+        priceDayTextView.setText(priceDayStrings[i]);
+
+        TextView priceHoliTextView = (TextView) view1.findViewById(R.id.textView12);
+        priceHoliTextView.setText(priceHolidayStrings[i]);
+
+
+        return view1;
+    }
+}   // Main Class
--- app/src/main/java/appewtc/masterung/bookmeetingpbru/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/bookmeetingpbru/ServiceActivity.java
@@ -1,13 +1,65 @@
 package appewtc.masterung.bookmeetingpbru;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ListView listView;
+    private String urlJSON = "http://swiftcodingthai.com/pbru/get_service.php";
+    private String[] nameRoomStrings, nameBuildStrings, sizeStrings,
+            priceDayStrings, priceHoliStrings, iconStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        //Bind Widget
+        textView = (TextView) findViewById(R.id.textView7);
+        listView = (ListView) findViewById(R.id.listView);
+
+        SynService synService = new SynService();
+        synService.execute();
+
+    }   // Main Method
+
+    public class SynService extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("pbruV2", "JSON ==> " + s);
+
+
+        }
+    }   // SynService Class
+
+}   // Main Class
