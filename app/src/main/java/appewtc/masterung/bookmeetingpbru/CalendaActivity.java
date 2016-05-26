a27ac0734043be0c589e2063a2909917b18f41ec
masterUNG <phrombutr@gmail.com>
2016-05-26 11:42:57 +0700

    Create Spinner

--- app/src/main/java/appewtc/masterung/bookmeetingpbru/CalendaActivity.java
+++ app/src/main/java/appewtc/masterung/bookmeetingpbru/CalendaActivity.java
@@ -1,13 +1,57 @@
 package appewtc.masterung.bookmeetingpbru;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class CalendaActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private Spinner spinner;
+    private RadioGroup radioGroup;
+    private RadioButton beforeNoonRadioButton, afterNoonRadioButton,
+            fullDayRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calenda);
+
+        bindWidget();
+
+        createSpinner();
+
+
+    }   // Main Method
+
+    private void createSpinner() {
+
+        String[] strAmount = {"1 วัน", "2 วัน", "3 วัน", "4 วัน", "5 วัน",
+                "6 วัน", "7 วัน", "8 วัน", "9 วัน", "10 วัน"};
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, strAmount);
+        spinner.setAdapter(stringArrayAdapter);
+
+
+    }   // createSpinner
+
+    private void bindWidget() {
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        radioGroup = (RadioGroup) findViewById(R.id.ragTime);
+        beforeNoonRadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        afterNoonRadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        fullDayRadioButton = (RadioButton) findViewById(R.id.radioButton5);
     }
-}
+
+    public void clickOrderCalendar(View view) {
+
+
+    }   // clickOrder
+
+}   // Main Class
